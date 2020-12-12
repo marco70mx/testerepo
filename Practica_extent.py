27ac5c24f958b0e6494b676b1f4fe3a6604eddb3
marco70mx <75866820+marco70mx@users.noreply.github.com>
2020-12-11 18:26:32 -0600

    Delete Practica_extent.py

--- Practica_extent.py
+++
@@ -1,9 +0,0 @@
-import arcpy.mapping as mapping
-mxd = mapping.MapDocument("CURRENT")
-for df in mapping.ListDataFrames(mxd):
-    if (df.name == 'Crime'):
-        layers = mapping.ListLayers(mxd,'Crime Density by School District',df)
-        for layer in layers:
-            query = '"NAME" = \'Lackland ISD\''
-            layer.definitionQuery = query
-            df.extent = layer.getExtent()
